78fcda625350b6852a1bb85d4937e5763975ec2e
Jacob schmidt <innovativestudios@outlook.com>
2023-06-17 17:49:53 -0500

    Hotfix 06172023@1750

--- addons/mercs/CfgVehicles.hpp
+++ addons/mercs/CfgVehicles.hpp
@@ -27,7 +27,7 @@ class CfgVehicles {
         // editorPreview = "";
 
         class Eventhandlers: Eventhandlers {
-            init = "[(_this select 0), ""default""] call sog_factions_pmc_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
+            init = "[(_this select 0), ""default""] call factions_mercs_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
         };
     };
     class CLASS(I_Rifleman_01_F): CLASS(I_TeamLeader_01_F) {
@@ -43,13 +43,13 @@ class CfgVehicles {
         // editorPreview = "";
 
         class Eventhandlers: Eventhandlers {
-            init = "[(_this select 0), ""default""] call sog_factions_pmc_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
+            init = "[(_this select 0), ""default""] call factions_mercs_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
         };
     };
     class CLASS(I_Rifleman_02_F): CLASS(I_TeamLeader_01_F) {
         displayName = "Rifleman";
         weapons[] = {"CUP_arifle_G3A3_ris_vfg", "CUP_hgun_Glock17_blk", "Throw", "Put"};
-        respawnWeapons[] = {"CUP_arifle_G3A3_ris_vfg", "CUP_17Rnd_9x19_glock17", "Throw", "Put"};
+        respawnWeapons[] = {"CUP_arifle_G3A3_ris_vfg", "CUP_hgun_Glock17_blk", "Throw", "Put"};
         magazines[] = {MAG_6("CUP_20Rnd_762x51_G3"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), "SmokeShellGreen"};
         respawnMagazines[] = {MAG_6("CUP_20Rnd_762x51_G3"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), "SmokeShellGreen"};
         linkedItems[] = {"CUP_H_PMC_EP_Headset", "G_Balaclava_blk_lxWS", "V_PlateCarrier1_blk", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "CUP_NVG_PVS7_Hide"};
@@ -59,15 +59,15 @@ class CfgVehicles {
         // editorPreview = "";
 
         class Eventhandlers: Eventhandlers {
-            init = "[(_this select 0), ""default""] call sog_factions_pmc_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
+            init = "[(_this select 0), ""default""] call factions_mercs_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
         };        
     };
     class CLASS(I_Grenadier_01_F): CLASS(I_TeamLeader_01_F) {
         displayName = "Grenadier";
         weapons[] = {"CUP_arifle_M4A1_BUIS_GL", "CUP_hgun_Glock17_blk", "Throw", "Put"};
-        respawnWeapons[] = {"CUP_arifle_M4A1_BUIS_GL", "CUP_17Rnd_9x19_glock17", "Throw", "Put"};
-        magazines[] = {MAG_6("CUP_30Rnd_556x45_Stanag"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), MAG_5("1Rnd_HE_Grenade_shell"), MAG_2("1Rnd_Smoke_Grenade_shell"), "1Rnd_SmokeRed_Grenade_shell", "SmokeShellGreen"};
-        respawnMagazines[] = {MAG_6("CUP_30Rnd_556x45_Stanag"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), MAG_5("1Rnd_HE_Grenade_shell"), MAG_2("1Rnd_Smoke_Grenade_shell"), "1Rnd_SmokeRed_Grenade_shell", "SmokeShellGreen"};
+        respawnWeapons[] = {"CUP_arifle_M4A1_BUIS_GL", "CUP_hgun_Glock17_blk", "Throw", "Put"};
+        magazines[] = {};
+        respawnMagazines[] = {};
         linkedItems[] = {"CUP_H_OpsCore_Black", "CUP_RUS_Balaclava_blk", "V_PlateCarrier1_blk", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "CUP_NVG_PVS7_Hide"};
         respawnLinkedItems[] = {"CUP_H_OpsCore_Black", "CUP_RUS_Balaclava_blk", "V_PlateCarrier1_blk", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "CUP_NVG_PVS7_Hide"};
         uniformClass = "eou_gorka_3";
@@ -75,13 +75,13 @@ class CfgVehicles {
         // editorPreview = "";
 
         class Eventhandlers: Eventhandlers {
-            init = "[(_this select 0), ""default""] call sog_factions_pmc_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
+            init = "[(_this select 0), ""default""] call factions_mercs_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
         };
     };
     class CLASS(I_Combat_Life_Saver_01_F): CLASS(I_TeamLeader_01_F) {
         displayName = "Combat Life Saver";
         weapons[] = {"CUP_arifle_L85A2_G", "CUP_hgun_Glock17_blk", "Throw", "Put"};
-        respawnWeapons[] = {"CUP_arifle_L85A2_G", "CUP_17Rnd_9x19_glock17", "Throw", "Put"};
+        respawnWeapons[] = {"CUP_arifle_L85A2_G", "CUP_hgun_Glock17_blk", "Throw", "Put"};
         magazines[] = {MAG_6("CUP_30Rnd_556x45_Stanag_L85"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), "SmokeShellGreen"};
         respawnMagazines[] = {MAG_6("CUP_30Rnd_556x45_Stanag_L85"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), "SmokeShellGreen"};
         linkedItems[] = {"CUP_H_PMC_EP_Headset", "G_Balaclava_blk_lxWS", "V_PlateCarrier1_blk", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "CUP_NVG_PVS7_Hide"};
@@ -93,15 +93,15 @@ class CfgVehicles {
         // editorPreview = "";
 
         class Eventhandlers: Eventhandlers {
-            init = "[(_this select 0), ""default""] call sog_factions_pmc_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
+            init = "[(_this select 0), ""default""] call factions_mercs_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
         };
     };
     class CLASS(I_Marksman_01_F): CLASS(I_TeamLeader_01_F) {
         displayName = "Marksman";
         weapons[] = {"CUP_srifle_M107_Base", "CUP_hgun_Glock17_blk", "Throw", "Put"};
-        respawnWeapons[] = {"CUP_srifle_M107_Base", "CUP_17Rnd_9x19_glock17", "Throw", "Put"};
-        magazines[] = {MAG_6("CUP_10Rnd_127x99_M107"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), "SmokeShellGreen"};
-        respawnMagazines[] = {MAG_6("CUP_10Rnd_127x99_M107"), MAG_2("CUP_17Rnd_9x19_glock17"), MAG_2("HandGrenade"), MAG_2("SmokeShell"), "SmokeShellGreen"};
+        respawnWeapons[] = {"CUP_srifle_M107_Base", "CUP_hgun_Glock17_blk", "Throw", "Put"};
+        magazines[] = {};
+        respawnMagazines[] = {};
         linkedItems[] = {"CUP_H_PMC_Cap_EP_Grey", "G_Bandanna_blk", "V_SmershVest_01_black_F", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "CUP_NVG_PVS7_Hide"};
         respawnLinkedItems[] = {"CUP_H_PMC_Cap_EP_Grey", "G_Bandanna_blk", "V_SmershVest_01_black_F", "ItemMap", "ItemCompass", "ItemWatch", "ItemRadio", "CUP_NVG_PVS7_Hide"};
         uniformClass = "eou_gorka_1";
@@ -109,7 +109,7 @@ class CfgVehicles {
         // editorPreview = "";
 
         class Eventhandlers: Eventhandlers {
-            init = "[(_this select 0), ""default""] call sog_factions_pmc_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
+            init = "[(_this select 0), ""marksman""] call factions_mercs_fnc_setUnitLoadout; [(_this select 0), nil, nil] call BIS_fnc_unitHeadgear;";
         };
     };
 };
